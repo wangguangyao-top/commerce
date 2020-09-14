939fbff97f3575442a1ea9c9242f8ac49d70cc00
Liar <1930804855@qq.com>
2020-09-14 11:41:38 +0800

    上传laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2组
+                    Laravel
                 </div>
 
                 <div class="links">
