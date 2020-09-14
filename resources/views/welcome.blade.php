168b83311e18878f277688cb379ce4053ee87623
Liar <1930804855@qq.com>
2020-09-14 11:40:03 +0800

    git 练习

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 2组
                 </div>
 
                 <div class="links">
