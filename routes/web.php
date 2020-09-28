2fe1c4d450e6f66752b1d90e8c7d5a5f59725c73
liuxinyue <1479450158@qq.com>
2020-09-28 14:53:29 +0800

    解决冲突

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,11 @@ Route::prefix('index')->group(function () {
     Route::any('/GoodsParticulars', 'index\ParticularsController@particulars');
     Route::any('/shopping', 'index\ShoppingController@shopping');
     Route::any('/cooperation', 'index\CooperationController@cooperation');
-<<<<<<< HEAD
     //前台支付失败
     Route::any('/payfail','index\PayController@payfail');
-=======
     Route::any('/reg','index\RegisterController@reg');
     Route::any('/sendSmsCode','index\RegisterController@sendSmsCode');
     Route::any('/code','index\RegisterController@code');
-
->>>>>>> c1cbd8eea2ef4f5f9f163ca9c882d4f23c26c69a
     //商品详情页
     Route::get('item','index\ItemController@item');
 
