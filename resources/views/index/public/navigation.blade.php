d1b26628f886c8ef94acf819ea0a67651ae4c916
liuxinyue <1479450158@qq.com>
2020-10-06 10:25:44 +0800

    21

--- resources/views/index/public/navigation.blade.php
+++ resources/views/index/public/navigation.blade.php
@@ -5,9 +5,7 @@
     <div class="yui3-u Center navArea">
         <ul class="nav">
 
-           @foreach($navdata as $v)
-              <li class="f-item">{{$v->nav_name}}</li>
-          @endforeach
+           
 
             <li class="f-item"><a href="seckill-index.html" target="_blank">秒杀</a></li>
         </ul>
