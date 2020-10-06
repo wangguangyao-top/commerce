31deadfec64ac873b9e0667b2a9d57c7ecd5a14f
xieyufei <a1678029050@163.com>
2020-10-06 13:33:42 +0800

    详情跳转

--- resources/views/index/list.blade.php
+++ resources/views/index/list.blade.php
@@ -233,7 +233,7 @@
                             <li class="yui3-u-1-5">
                                 <div class="list-wrap">
                                     <div class="p-img">
-                                        <a href="" target="_blank"><img src="{{$v->goods_img[0]}}"></a>
+                                        <a href="/index/item?goods_id={{$v->goods_id}}" target="_blank"><img src="{{$v->goods_img[0]}}"></a>
                                     </div>
                                     <div class="price">
                                         <strong>
