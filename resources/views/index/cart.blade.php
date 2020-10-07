81bbeb2ab408cb6350fbf11db60158d92ce0fda8
Liar <1930804855@qq.com>
2020-10-07 13:50:55 +0800

    修改

--- resources/views/index/cart.blade.php
+++ resources/views/index/cart.blade.php
@@ -18,12 +18,22 @@
         <div class="shortcut">
             <ul class="fl">
                 <li class="f-item">品优购欢迎您！</li>
-                <li class="f-item">请登录　<span><a href="#">免费注册</a></span></li>
+                @php
+                $user=json_decode(session('user'),true);
+                @endphp
+                @if(empty($user))
+                    <li class="f-item">请先<a href="{{url('index/login')}}">登录</a>　<span><a href="{{url('index/register')}}">免费注册</a></span></li>
+                @else
+                    <li class="f-item"><span><a>{{$user['user_name']}}</a></span>&nbsp;|&nbsp;<a href="{{url('index/quit')}}">退出</a></li>
+                @endif
+
             </ul>
             <ul class="fr">
-                <li class="f-item">我的订单</li>
-                <li class="f-item space"></li>
-                <li class="f-item"><a href="{{url('index/orderHome')}}">我的品优购</a></li>
+                @if(!empty($user))
+                    <li class="f-item"><a href="{{url('index/order')}}">我的订单</a></li>
+                    <li class="f-item space"></li>
+                @endif
+                <li class="f-item"><a href="home.html" target="_blank">我的品优购</a></li>
                 <li class="f-item space"></li>
                 <li class="f-item">品优购会员</li>
                 <li class="f-item space"></li>
@@ -31,13 +41,22 @@
                 <li class="f-item space"></li>
                 <li class="f-item">关注品优购</li>
                 <li class="f-item space"></li>
-                <li class="f-item">客户服务</li>
+                <li class="f-item" id="service">
+                    <span>客户服务</span>
+                    <ul class="service">
+                        <li><a href="cooperation.html" target="_blank">合作招商</a></li>
+                        <li><a href="shoplogin.html" target="_blank">商家后台</a></li>
+                        <li><a href="cooperation.html" target="_blank">合作招商</a></li>
+                        <li><a href="#">商家后台</a></li>
+                    </ul>
+                </li>
                 <li class="f-item space"></li>
                 <li class="f-item">网站导航</li>
             </ul>
         </div>
     </div>
 </div>
+
 <div class="cart py-container">
     <!--logoArea-->
     <div class="logoArea">
@@ -85,9 +104,10 @@
 
                             <li class="yui3-u-1-8"><span class="price">{{$v->goods_price}}</span></li>
                             <li class="yui3-u-1-8">
-                                <a href="javascript:void(0)" class="increment mins">-</a>
-                                <input autocomplete="off" type="text" value="{{$v->cart_nums}}" minnum="1" class="itxt" />
-                                <a href="javascript:void(0)" class="increment plus">+</a>
+                                <span class="price">{{$v->cart_nums}}</span>
+                                {{--<a href="javascript:void(0)" class="increment mins">-</a>--}}
+                                {{--<input autocomplete="off" type="text" value="{{$v->cart_nums}}" minnum="1" class="itxt" />--}}
+                                {{--<a href="javascript:void(0)" class="increment plus">+</a>--}}
                             </li>
                             <li class="yui3-u-1-8"><span class="sum">{{$v->subtotal}}</span></li>
                             <li class="yui3-u-1-8">
